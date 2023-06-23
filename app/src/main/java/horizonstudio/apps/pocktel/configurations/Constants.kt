1a43cd040c0631885370c8782f21a8314cec45b7
Ofek Reuveni <ofekrv@gmail.com>
2023-06-23 17:03:24 +0300

    result fragment reshaped

--- app/src/main/java/horizonstudio/apps/pocktel/configurations/Constants.kt
+++ app/src/main/java/horizonstudio/apps/pocktel/configurations/Constants.kt
@@ -4,7 +4,7 @@ import horizonstudio.apps.pocktel.dal.entities.RuleSet
 
 object Constants {
     const val DATABASE_NAME:String = "pocktel"
-    const val BASE_URL:String = "http://10.0.0.9:8080/"
+    const val BASE_URL:String = "https://yarapi-a2zphhdpma-uc.a.run.app"
     const val MAX_TRIES: Int = 20
     const val TIME_INTERVAL: Long = 500
     const val FILENAME_ARGUMENT_NAME: String = "filename"
